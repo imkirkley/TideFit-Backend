769212e717ab6a8e0d954c8a24b6b8385168da57
imkirkley <imkirkley@crimson.ua.edu>
2023-11-17 15:11:15 -0600

    getting close

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -2,7 +2,7 @@ namespace api.Models
 {
     public class Activity
     {
-        public string ExerciseId {get; set;}
+        public string Id {get; set;}
         public string ActivityType {get; set;}
         public string Distance {get; set;}
         public string DateCompleted {get; set;}
@@ -10,7 +10,7 @@ namespace api.Models
         public bool Deleted {get; set;}
         
         public Activity() {
-            ExerciseId = Guid.NewGuid().ToString();
+            Id = Guid.NewGuid().ToString();
         }
 
 
